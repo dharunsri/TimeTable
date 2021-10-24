1bf70ab7c0e8c20e210274276552bf8d0e8b7902
Tech Boy <67187601+dharunsri@users.noreply.github.com>
2021-10-24 22:50:25 +0530

    Time Table App

--- app/src/main/java/com/innovators/timetable/Update_Reminder_Notes.java
+++ app/src/main/java/com/innovators/timetable/Update_Reminder_Notes.java
@@ -2,19 +2,30 @@ package com.innovators.timetable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class Update_Reminder_Notes extends AppCompatActivity {
 
     EditText title,description;
+    TextView reminder_date;
+    ImageButton reminder_bt;
     Button updateNotes;
     String id;
+    String date_time = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +37,21 @@ public class Update_Reminder_Notes extends AppCompatActivity {
         title = findViewById(R.id.r_title);
         description = findViewById(R.id.r_description);
         updateNotes = findViewById(R.id.update_reminder_bt);
+        reminder_bt = findViewById(R.id.reminder);
+        reminder_date = findViewById(R.id.reminder_date);
 
         Intent i =getIntent();
         title.setText(i.getStringExtra("title"));
         description.setText(i.getStringExtra("description"));
         id=i.getStringExtra("id");
+
+        reminder_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePicker();
+
+            }
+        });
 
         updateNotes.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,5 +76,39 @@ public class Update_Reminder_Notes extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void datePicker(){
+        final Calendar cal = Calendar.getInstance();
+        int Year = cal.get(Calendar.YEAR);
+        int Month = cal.get(Calendar.MONTH);
+        int Day = cal.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                date_time = dayOfMonth + "-" + (month + 1) + "-" + year;
+                timePicker();
+
+            }
+        }, Year,Month,Day);
+        datePickerDialog.show();
+    }
+
+    private void timePicker(){
+        final Calendar cal = Calendar.getInstance();
+        int Hour = cal.get(Calendar.HOUR_OF_DAY);
+        int Minute = cal.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                int Hour = hourOfDay;
+                int Minute = minute;
+
+                reminder_date.setText(date_time+" ("+hourOfDay+":"+minute+")");
+            }
+        }, Hour, Minute, false);
+        timePickerDialog.show();
     }
 }
--- app/src/main/java/com/innovators/timetable/add_reminders.java
+++ app/src/main/java/com/innovators/timetable/add_reminders.java
@@ -2,18 +2,30 @@ package com.innovators.timetable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class add_reminders extends AppCompatActivity {
 
     EditText title, description;
+    TextView reminder_date;
     Button addReminder;
+    ImageButton reminder_bt;
+
+    String date_time = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +37,16 @@ public class add_reminders extends AppCompatActivity {
         title = findViewById(R.id.r_title);
         description = findViewById(R.id.r_description);
         addReminder = findViewById(R.id.add_reminder_bt);
+        reminder_bt = findViewById(R.id.reminder);
+        reminder_date = findViewById(R.id.reminder_date);
+
+        reminder_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePicker();
+
+            }
+        });
 
         addReminder.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,5 +66,39 @@ public class add_reminders extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void datePicker(){
+        final Calendar cal = Calendar.getInstance();
+        int Year = cal.get(Calendar.YEAR);
+        int Month = cal.get(Calendar.MONTH);
+        int Day = cal.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                date_time = dayOfMonth + "-" + (month + 1) + "-" + year;
+                timePicker();
+
+            }
+        }, Year,Month,Day);
+        datePickerDialog.show();
+    }
+
+    private void timePicker(){
+        final Calendar cal = Calendar.getInstance();
+        int Hour = cal.get(Calendar.HOUR_OF_DAY);
+        int Minute = cal.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                int Hour = hourOfDay;
+                int Minute = minute;
+
+                reminder_date.setText(date_time+" ("+hourOfDay+":"+minute+")");
+            }
+        }, Hour, Minute, false);
+        timePickerDialog.show();
     }
 }
